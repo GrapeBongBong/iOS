dafb15920d75e2f33141ba23832dd1116cf6bab8
newJunsung <assd1108@naver.com>
2023-03-19 20:40:09 +0900

    Initial Commit

---
+++ GrapeBongBong/GrapeBongBongApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrapeBongBongApp.swift
+//  GrapeBongBong
+//
+//  Created by 김준성 on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct GrapeBongBongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
